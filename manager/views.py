ec174fc05fe2ff070f08b467940d0a0f3a06690f
damondeng <304749970@qq.com>
2018-08-26 22:07:43 +0800

    基于django-adminlte2

---
+++ manager/views.py
@@ -0,0 +1,18 @@
+import logging
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+
+
+LOGGER = logging.getLogger('__name__')
+
+
+@login_required()
+def index(request):
+    LOGGER.info("start application")
+    LOGGER.error("fail to start application")
+    return redirect('/website/')
+
+
+@login_required()
+def noperm(request):
+    return render(request, 'noperm.html', {"user": request.user})
